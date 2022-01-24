285588cb9ddc587b2a2523e9eebb68c6a948d8de
BaltaJmn <baltax.75@gmail.com>
2022-01-24 13:20:55 +0100

    Adds view pager with all the fragments

--- app/src/main/java/es/baltajmn/tappapp/MainActivity.kt
+++ app/src/main/java/es/baltajmn/tappapp/MainActivity.kt
@@ -1,11 +1,56 @@
 package es.baltajmn.tappapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import es.baltajmn.tappapp.databinding.ActivityMainBinding
+import es.baltajmn.tappapp.fragment.OrderFragment
+import es.baltajmn.tappapp.fragment.TotalFragment
+import es.baltajmn.tappapp.fragment.UserFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val wormsIndicator = binding.dotsIndicator
+        val viewPager = binding.viewPager
+        val adapter = ViewPagerAdapter(this)
+
+        adapter.addFragment(UserFragment(), "User")
+        adapter.addFragment(OrderFragment(), "Order")
+        adapter.addFragment(TotalFragment(), "Total")
+
+        viewPager.adapter = adapter
+        wormsIndicator.setViewPager2(viewPager)
+
+    }
+
+    private inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItemCount(): Int {
+            return mFragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        fun getPageTitle(position: Int): CharSequence? {
+            return mFragmentTitleList[position]
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
     }
 }
